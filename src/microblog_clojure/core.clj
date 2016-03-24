dee57df95031e01eacbd4f301faa1e313c491b16
BrandenSandahl <branden157@gmail.com>
2016-03-24 11:12:40 -0400

    set up server

--- src/microblog_clojure/core.clj
+++ src/microblog_clojure/core.clj
@@ -1,5 +1,20 @@
 (ns microblog-clojure.core
+  (:require [clojure.string :as str] ;this brings in a new lib
+            [clojure.walk :as walk] ;brings in walk lib 
+            [compojure.core :as c]
+            [ring.adapter.jetty :as j]
+            [ring.middleware.params :as p]
+            [hiccup.core :as h])
   (:gen-class))
 
+(c/defroutes app
+  (c/GET "/" request
+    "Hello, world!"))
+
+
+(defonce server (atom nil))
+
 (defn -main []
-  (println "Hello, World!"))
+  (when @server
+    (.stop @server))
+  (reset! server j/run-jetty app (:port 3000 :join? false)))
